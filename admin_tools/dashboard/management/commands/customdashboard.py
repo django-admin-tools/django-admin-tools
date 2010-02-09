d764e99974bd2ce69bdd4217a296f977bea53f3d
David Jean Louis <izimobil@gmail.com>
2010-02-09 01:03:40 +0100

    make sure management commands work as expected

--- admin_tools/dashboard/management/commands/customdashboard.py
+++ admin_tools/dashboard/management/commands/customdashboard.py
@@ -7,36 +7,19 @@ from django.template.loader import render_to_string
 DEFAULT_FILE = 'dashboard.py'
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option(
-            '-f',
-            '--file',
-            help='The file path where the base code should be written (default: dashboard.py)',
-            default=DEFAULT_FILE,
-        ),
-    )
     help = ('Creates a template file containing the base code to get you '
             'started with your custom dashboard.')
-    args = '[appname]'
+    args = '[file]'
     label = 'application name'
 
-    def handle(self, appname=None, **options):
+    def handle(self, file=None, **options):
         context = {}
         context['project'] = os.path.basename(os.getcwd())
-        if appname:
-            tpl = 'dashboard/dashboard_app_index.txt'
-            if not os.path.exists(appname):
-                raise CommandError('application "%s" not found' % appname)
-            dst = os.path.join(appname, options['file'])
-            context['app'] = appname
-            if options['file'] != DEFAULT_FILE:
-                context['warning'] = True
-        else:
-            tpl = 'dashboard/dashboard.txt'
-            dst = os.path.join(options['file'])
+        tpl = 'dashboard/dashboard.txt'
+        dst = file is not None and file or DEFAULT_FILE
         if os.path.exists(dst):
             raise CommandError('file "%s" already exists' % dst)
         context['file'] = os.path.basename(dst).split('.')[0]
         open(dst, 'w').write(render_to_string(tpl, context))
-        print '"%s" written.' % os.path.join(options['file'])
+        print '"%s" written.' % os.path.join(dst)
 
--- admin_tools/menu/management/commands/custommenu.py
+++ admin_tools/menu/management/commands/custommenu.py
@@ -4,26 +4,21 @@ from django.core.management.base import BaseCommand, CommandError
 from django.template.loader import render_to_string
 
 
+DEFAULT_FILE = 'menu.py'
+
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option(
-            '-f',
-            '--file',
-            help='The file path where the base code should be written (default: menu.py)',
-            default='menu.py',
-        ),
-    )
     help = ('Creates a template file containing the base code to get you '
             'started with your custom menu')
+    args = ['file']
 
-    def handle(self, **options):
+    def handle(self, file=None, **options):
         project_name = os.path.basename(os.getcwd())
-        dst = os.path.join(options['file'])
+        dst = file is not None and file or DEFAULT_FILE
         if os.path.exists(dst):
             raise CommandError('Error: file "%s" already exists' % dst)
         open(dst, 'w').write(render_to_string('menu/menu.txt', {
             'project': project_name,
-            'file': os.path.basename(options['file']).split('.')[0]
+            'file': os.path.basename(dst).split('.')[0]
         }))
-        print '"%s" written.' % os.path.join(options['file'])
+        print '"%s" written.' % os.path.join(dst)
 
