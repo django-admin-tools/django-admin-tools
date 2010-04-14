e3bb8f7f07fe1b5bcf711001ae89093e00d62919
David Jean Louis <izimobil@gmail.com>
2010-04-14 17:54:19 +0200

    build urlpatterns conditionally regarding the content of INSTALLED_APPS

--- admin_tools/urls.py
+++ admin_tools/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, url, include
 
-urlpatterns = patterns('',
-    url(r'^menu/', include('admin_tools.menu.urls')),
-    url(r'^dashboard/', include('admin_tools.dashboard.urls')),
-)
+urls = []
+if 'admin_tools.menu' in settings.INSTALLED_APPS:
+    urls.append(url(r'^menu/', include('admin_tools.menu.urls')))
+if 'admin_tools.dashboard' in settings.INSTALLED_APPS:
+    urls.append(url(r'^dashboard/', include('admin_tools.dashboard.urls')))
+
+urlpatterns = patterns('', *urls)
