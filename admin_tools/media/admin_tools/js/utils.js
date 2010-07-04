2cda0e2584765edafa567d3b664edf90296119eb
Mike Korobov <kmike84@gmail.com>
2010-07-04 14:50:33 +0600

    Execute initialization javascript in callbacks to make sure that necessary scripts are loaded. See #32.

--- admin_tools/media/admin_tools/js/utils.js
+++ admin_tools/media/admin_tools/js/utils.js
@@ -1,17 +1,30 @@
-var load_script = function(index, js_files) {
-    if (typeof(js_files[index]) != 'undefined') {
-        if (js_files[index].test()) {
-            // add script element
-            //console.log('Loading ' + js_files[index].src);
-            var head = document.getElementsByTagName('head')[0];
+var loadScripts = function(js_files, onComplete){
+    var len = js_files.length;
+    var head = document.getElementsByTagName('head')[0];
+
+    function loadScript(index){
+
+        if (index >= len){
+            onComplete();
+            return;
+        }
+
+        if (js_files[index].test()){
+//            console.log('Loading ' + js_files[index].src);
+
             var s = document.createElement('script');
             s.src = js_files[index].src;
             s.type = 'text/javascript';
             head.appendChild(s);
-            s.onload = load_script(index + 1, js_files);
-        } else {
-            //console.log('Skipped already loaded file ' + js_files[index].src);
-            load_script(index + 1, js_files);
+
+            s.onload = function(){
+                loadScript(index+1);
+            }
+        }
+        else{
+            loadScript(index+1);
         }
     }
+
+    loadScript(0);
 }
