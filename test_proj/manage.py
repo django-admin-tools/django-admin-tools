2158010362c18eb6b6bcb23e1994a6130e279a22
David Jean Louis <izimobil@gmail.com>
2010-10-02 11:39:10 +0200

    Fixed issue #51: more robust test running (thanks to Tom von Schwerdtner for the idea)

--- test_proj/manage.py
+++ test_proj/manage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+
+import os
+import sys
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, path)
+
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
-    import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
