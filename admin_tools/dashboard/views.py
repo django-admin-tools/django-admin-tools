d9920881f071db0ab1ca2c30b3c10d01dcb5291f
Victor Safronovich <vsafronovich@gmail.com>
2012-02-07 13:14:26 +0500

    misprint fixed

--- admin_tools/dashboard/views.py
+++ admin_tools/dashboard/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import RequestContext
-from django.shortcuts import render_to_responce
+from django.shortcuts import render_to_response
 
 try:
     from django.views.decorators.csrf import csrf_exempt
