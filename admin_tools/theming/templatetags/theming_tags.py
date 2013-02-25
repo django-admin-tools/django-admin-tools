796abb26e21a9548e82756adbb893e45cc1053c6
David Jean Louis <izimobil@gmail.com>
2013-02-25 23:07:42 +0100

    Fixed django 1.3 support

--- admin_tools/theming/templatetags/theming_tags.py
+++ admin_tools/theming/templatetags/theming_tags.py
@@ -21,3 +21,15 @@ def render_theming_css():
         css = '/'.join([get_media_url(), 'admin_tools', 'css', 'theming.css'])
     return '<link rel="stylesheet" type="text/css" media="screen" href="%s" />' % css
 register.simple_tag(render_theming_css)
+
+
+def get_admin_media(media=''):
+    """
+    Template tag that renders the needed css files for the theming app.
+    """
+    return getattr(
+        settings,
+        'ADMIN_MEDIA_PREFIX',  # django 1.3
+        '%sadmin/' % getattr(settings, 'STATIC_URL')  # django > 1.4
+    ) + media
+register.simple_tag(get_admin_media)
