e1432bb03b059887be11dbf5b7261bcdd7e83ce6
David Jean Louis <izimobil@gmail.com>
2013-02-25 15:50:54 +0100

    Fixed tests

--- test_proj/settings.py
+++ test_proj/settings.py
@@ -1,56 +1,180 @@
 # settings for django-admin-tools test project.
-import os
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'static')
-MEDIA_URL = '/static/'
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = 'testdb.sqlite'
-SITE_ID = 1
-DEBUG = True
 
+import os
+import sys
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(PROJECT_PATH))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'testdb.sqlite',               # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'America/Chicago'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = False
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = False
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = False
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '6(ogp5)w8^ztc^%6*3!47vb_tc4!*tj3tfmv0s*s2z8^^ayz2v'
+
+# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+ROOT_URLCONF = 'test_proj.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'test_proj.wsgi.application'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
     'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
 )
 
-ROOT_URLCONF = 'urls'
-
 TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
     PROJECT_PATH + '/templates',
 )
 
-FIXTURE_DIRS = ['fixtures']
-
-ADMIN_TOOLS_INDEX_DASHBOARD = 'test_proj.dashboard.CustomIndexDashboard'
-ADMIN_TOOLS_MENU = 'test_proj.menu.CustomMenu'
-
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     'admin_tools',
-    'admin_tools.theming',
-    'admin_tools.menu',
     'admin_tools.dashboard',
-
+    'admin_tools.menu',
+    'admin_tools.theming',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
     'south',
-
     'test_app',
-]
+)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+SECRET_KEY = 'django-admin-tools'
 
 try:
     import django_coverage
@@ -58,3 +182,6 @@ try:
     COVERAGE_REPORT_HTML_OUTPUT_DIR = os.path.join(PROJECT_PATH, '_coverage')
 except ImportError:
     pass
+
+ADMIN_TOOLS_INDEX_DASHBOARD = 'test_proj.dashboard.CustomIndexDashboard'
+ADMIN_TOOLS_MENU = 'test_proj.menu.CustomMenu'
--- test_proj/test_app/tests.py
+++ test_proj/test_app/tests.py
@@ -1,31 +1,43 @@
 from django.test import TestCase
+import sys
 
 class AdminBasicTest(TestCase):
 
-    def index_page(self, username='staff', password='123'):
-        self.assertTrue(self.client.login(username=username, password=password))
-        return self.client.get('/admin/')
+    fixtures = ['initial_data.json']
 
     def test_admin_loads(self):
-        self.assertEqual(self.index_page().status_code, 200)
+        for (username, password) in (('superuser', '123'), ('staff', '123')):
+            logged_in = self.client.login(username=username, password=password)
+            self.assertTrue(logged_in)
+            response = self.client.get('/admin/')
+            self.assertEqual(response.status_code, 200)
+            self.client.logout()
 
     def test_permissions(self):
-        index = self.index_page()
-        self.assertContains(index, 'Foos')
-        self.assertNotContains(index, 'Bars')
+        self.client.login(username='staff', password='123')
+        index = self.client.get('/admin/')
+        self.assertContains(index, 'Bars')
+        self.assertNotContains(index, 'Foos')
         self.assertNotContains(index, 'Users')
-        self.assertNotContains(index, 'Test app menu')
+        self.assertContains(index, 'Test app menu')
+        self.client.logout()
 
-        super_index = self.index_page('superuser', '123')
-        self.assertContains(super_index, 'Users', 3) # menu and dashboard items
+        self.client.login(username='superuser', password='123')
+        super_index = self.client.get('/admin/')
+        self.assertContains(super_index, 'Bars')
+        self.assertContains(super_index, 'Foos')
         self.assertContains(super_index, 'Test app menu')
+        self.assertContains(super_index, 'Users', 3) # menu and dashboard items
+        self.client.logout()
+        self.client.logout()
 
     def test_app_index(self):
         self.client.login(username='staff', password='123')
         res = self.client.get('/admin/test_app/')
         self.assertEqual(res.status_code, 200)
-        self.assertContains(res, 'Foos')
-        self.assertNotContains(res, 'Bars')
+        self.assertContains(res, 'Bars')
+        self.assertNotContains(res, 'Foos')
+        self.client.logout()
 
         self.client.login(username='superuser', password='123')
         res = self.client.get('/admin/test_app/')
