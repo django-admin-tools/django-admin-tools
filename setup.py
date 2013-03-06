625ad4651d06c7b1c74bcbd185f1a8b0011d136f
David Jean Louis <izimobil@gmail.com>
2013-03-06 11:47:57 +0100

    Simplified setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
 #!/usr/bin/env python
-from distutils.core import setup
-import os
+
+from setuptools import setup, find_packages
 from admin_tools import VERSION
-
-
-# taken from django-registration
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('admin_tools'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[12:] # Strip "admin_tools/" or "admin_tools\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
 
 bitbucket_url = 'http://bitbucket.org/izi/django-admin-tools/'
 long_desc = '''
@@ -36,16 +13,14 @@ long_desc = '''
 setup(
     name='django-admin-tools',
     version=VERSION.replace(' ', '-'),
-    description=('A collection of tools for the django administration '
-                 'interface'),
+    description='A collection of tools for the django administration interface',
     long_description=long_desc,
     author='David Jean Louis',
     author_email='izimobil@gmail.com',
     url=bitbucket_url,
     download_url='%sdownloads/django-admin-tools-%s.tar.gz' % (bitbucket_url, VERSION),
-    package_dir={'admin_tools': 'admin_tools'},
-    packages=packages,
-    package_data={'admin_tools': data_files},
+    packages=find_packages(),
+    include_package_data=True,
     license='MIT License',
     classifiers=[
         'Development Status :: 4 - Beta',
