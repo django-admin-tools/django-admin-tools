8a213db74b1177067e998f359d0c7beea553d7cf
hirokiky <hirokiky@gmail.com>
2013-12-08 17:29:20 +0900

    Adding tests for Django1.6 with Python2.7
    * Fixed tox
    * Fixed test_proj
    
    --HG--
    branch : supportdj16

--- test_proj/urls.py
+++ test_proj/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import url, patterns, include
+except ImportError:
+    from django.conf.urls.defaults import url, patterns, include
 from django.conf import settings
 from django.contrib import admin
 
