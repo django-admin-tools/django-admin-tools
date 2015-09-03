49b939daf219e6b6450a9db038ed4614fdb13629
David Jean Louis <izimobil@gmail.com>
2015-09-03 12:47:20 +0200

    Removed django dependency as it can cause problems (eg.: unwanted django upgrade)

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
-    install_requires=["django>=1.7"],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
