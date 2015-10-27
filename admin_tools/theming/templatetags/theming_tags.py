c87ccfa199f35f09b39099445d8441bff3d2c9d5
David Jean Louis <izimobil@gmail.com>
2015-10-27 10:30:01 +0100

    Added mark_safe() (needed for django 1.9)

--- admin_tools/theming/templatetags/theming_tags.py
+++ admin_tools/theming/templatetags/theming_tags.py
@@ -7,6 +7,7 @@ To load the theming tags just do: ``{% load theming_tags %}``.
 from django import template
 from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -17,5 +18,5 @@ def render_theming_css():
     css = getattr(settings, 'ADMIN_TOOLS_THEMING_CSS', False)
     if not css:
         css = '/'.join(['admin_tools', 'css', 'theming.css'])
-    return '<link rel="stylesheet" type="text/css" media="screen" href="%s" />' % staticfiles_storage.url(css)
+    return mark_safe('<link rel="stylesheet" type="text/css" media="screen" href="%s" />' % staticfiles_storage.url(css))
 register.simple_tag(render_theming_css)
