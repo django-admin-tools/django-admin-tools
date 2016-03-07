85368b0cea6f4649e43826be5945b1a117df4d03
Rohan de Swardt <rohanza@gmail.com>
2016-03-07 18:31:06 +0200

    fix csrf js

--- admin_tools/static/admin_tools/js/utils.js
+++ admin_tools/static/admin_tools/js/utils.js
@@ -1,6 +1,21 @@
-function isCsrfSafeMethod(method) {
+function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
 function useCsrfTokens(){
     // set CSRFToken header on POST requests (and remove the need for @csrf_exempt)
