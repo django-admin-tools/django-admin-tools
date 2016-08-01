2864b01fd3bc3bc69db08b74a3cdbf0a1b50b622
Petr Dlouhý <petr.dlouhy@email.cz>
2016-08-01 13:06:40 +0200

    fix tests in Django 1.10

--- test_proj/test_app/tests.py
+++ test_proj/test_app/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 import sys
 
 class AdminBasicTest(TestCase):
@@ -7,29 +8,45 @@ class AdminBasicTest(TestCase):
 
     def test_admin_loads(self):
         for (username, password) in (('superuser', '123'), ('staff', '123')):
-            logged_in = self.client.login(username=username, password=password)
-            self.assertTrue(logged_in)
+            try:
+                user = User.objects.get(username=username)
+                self.client.force_login(user)
+            except AttributeError:  # in Django<1.9
+                logged_in = self.client.login(username=username, password=password)
+                self.assertTrue(logged_in)
             response = self.client.get('/admin/')
             self.assertEqual(response.status_code, 200)
             self.client.logout()
 
 
     def test_custom_menu_media(self):
-        self.client.login(username='superuser', password='123')
+        try:
+            user = User.objects.get(username='superuser')
+            self.client.force_login(user)
+        except AttributeError:  # in Django<1.9
+            self.client.login(username='superuser', password='123')
         response = self.client.get('/admin/')
         self.assertContains(response, '<link rel="stylesheet" href="/static/test_app/menu.css" type="text/css" media="all"/>')
         self.assertContains(response, '/static/test_app/menu.js')
         self.client.logout()
 
     def test_custom_dashboard_media(self):
-        self.client.login(username='superuser', password='123')
+        try:
+            user = User.objects.get(username='superuser')
+            self.client.force_login(user)
+        except AttributeError:  # in Django<1.9
+            self.client.login(username='superuser', password='123')
         response = self.client.get('/admin/')
         self.assertContains(response, '<link rel="stylesheet" href="/static/test_app/dashboard.css" type="text/css" media="all"/>')
         self.assertContains(response, '/static/test_app/dashboard.js')
         self.client.logout()
 
     def test_permissions(self):
-        self.client.login(username='staff', password='123')
+        try:
+            user = User.objects.get(username='staff')
+            self.client.force_login(user)
+        except AttributeError:  # in Django<1.9
+            self.client.login(username='staff', password='123')
         index = self.client.get('/admin/')
         self.assertContains(index, 'Bars')
         self.assertNotContains(index, 'Foos')
@@ -37,7 +54,11 @@ class AdminBasicTest(TestCase):
         self.assertContains(index, 'Test app menu')
         self.client.logout()
 
-        self.client.login(username='superuser', password='123')
+        try:
+            user = User.objects.get(username='superuser')
+            self.client.force_login(user)
+        except AttributeError:  # in Django<1.9
+            self.client.login(username='superuser', password='123')
         super_index = self.client.get('/admin/')
         self.assertContains(super_index, 'Bars')
         self.assertContains(super_index, 'Foos')
@@ -47,14 +68,22 @@ class AdminBasicTest(TestCase):
         self.client.logout()
 
     def test_app_index(self):
-        self.client.login(username='staff', password='123')
+        try:
+            user = User.objects.get(username='staff')
+            self.client.force_login(user)
+        except AttributeError:  # in Django<1.9
+            self.client.login(username='staff', password='123')
         res = self.client.get('/admin/test_app/')
         self.assertEqual(res.status_code, 200)
         self.assertContains(res, 'Bars')
         self.assertNotContains(res, 'Foos')
         self.client.logout()
 
-        self.client.login(username='superuser', password='123')
+        try:
+            user = User.objects.get(username='superuser')
+            self.client.force_login(user)
+        except AttributeError:  # in Django<1.9
+            self.client.login(username='superuser', password='123')
         res = self.client.get('/admin/test_app/')
         self.assertContains(res, 'Foos')
         self.assertContains(res, 'Bars')
--- test_proj/urls.py
+++ test_proj/urls.py
@@ -1,14 +1,15 @@
 try:
-    from django.conf.urls import patterns, url, include
+    from django.conf.urls import url, include
 except ImportError: # django < 1.4
-    from django.conf.urls.defaults import patterns, url, include
+    from django.conf.urls.defaults import url, include
 from django.conf import settings
 from django.contrib import admin
+import django
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^admin_tools/', include('admin_tools.urls')),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-)
+    url(r'^static/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.MEDIA_ROOT}),
+]
