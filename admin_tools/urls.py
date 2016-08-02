7c012545da52019074c38536f63dee078c145858
Hamdi Sahloul <sahloul@race.u-tokyo.ac.jp>
2016-08-02 15:54:20 +0900

    Compatibility fix
    Patterns have been removed in Django 1.10

--- admin_tools/urls.py
+++ admin_tools/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 
 urlpatterns = []
 if 'admin_tools.menu' in settings.INSTALLED_APPS:
