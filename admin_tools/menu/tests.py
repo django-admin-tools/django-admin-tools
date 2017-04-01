b225c1d6ddd56b0d727c0dd5a773955a8d474126
Vasyl Dizhak <vasyl.dizhak@moneypark.ch>
2017-04-01 16:18:22 +0200

    #69, prevent removing artibitrary bookmarks by knowing only id

--- admin_tools/menu/tests.py
+++ admin_tools/menu/tests.py
@@ -1,8 +1,11 @@
 from tempfile import mktemp
 from django.test import TestCase
 from django.core import management
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 from admin_tools.menu.items import AppList
+from admin_tools.menu.models import Bookmark
 
 
 class ManagementCommandTest(TestCase):
@@ -17,6 +20,41 @@ class ManagementCommandTest(TestCase):
             assert False
         except:
             pass
+
+
+class DeleteBookMarkTest(TestCase):
+    fixtures = ['users.json']
+
+    def setUp(self):
+        self.superuser_credentials = ('superuser', '123')
+        self.staff_credentials = ('staff', '123')
+        user = User.objects.get(username=self.superuser_credentials[0])
+        self.bookmark = Bookmark.objects.create(
+            user=user,
+            url='/test/',
+            title='test'
+        )
+        self.delete_bookmark_url = reverse('admin-tools-menu-remove-bookmark', args=(self.bookmark.id,))
+
+    def _login_user(self, username, password):
+        try:
+            user = User.objects.get(username=username)
+            self.client.force_login(user)
+        except AttributeError:  # in Django<1.9
+            logged_in = self.client.login(username=user.username, password=password)
+            self.assertTrue(logged_in)
+
+    def test_removing_of_own_bookmark(self):
+        self.assertEqual(Bookmark.objects.count(), 1)
+        self._login_user(*self.superuser_credentials)
+        self.client.post(self.delete_bookmark_url)
+        self.assertFalse(Bookmark.objects.count())
+
+    def test_removing_others_bookmark(self):
+        self.assertEqual(Bookmark.objects.count(), 1)
+        self._login_user(*self.staff_credentials)
+        self.client.post(self.delete_bookmark_url)
+        self.assertEqual(Bookmark.objects.first(), self.bookmark)
 
 
 __test__ = {
